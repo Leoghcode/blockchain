23016f3375025a3a350429c2a9fdaa416c62b0f5
Leo.qi <qbzhhxx1996@qq.com>
2019-06-17 19:17:14 +0800

    增加从CA获取节点的功能

--- src/main/java/com/example/blockchain/controller/NodeController.java
+++ src/main/java/com/example/blockchain/controller/NodeController.java
@@ -1,6 +1,7 @@
 package com.example.blockchain.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.example.blockchain.Entity.CAEntity;
 import com.example.blockchain.Entity.Node;
 import com.example.blockchain.service.NodeService;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -31,5 +33,15 @@ public class NodeController {
     @RequestMapping(value = "get", method = RequestMethod.GET)
     public String getNodeNames() {
         return JSON.toJSONString(nodeService.getNodeNames());
+    }
+
+    @RequestMapping(value = "addFromCA", method = RequestMethod.GET)
+    public List<Node> addNodesFromCA() {
+        return nodeService.addNodesFromCA();
+    }
+
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public List<Node> getList() {
+        return nodeService.getNodeArrayList();
     }
 }
--- src/main/java/com/example/blockchain/service/NodeService.java
+++ src/main/java/com/example/blockchain/service/NodeService.java
@@ -1,9 +1,16 @@
 package com.example.blockchain.service;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.example.blockchain.Entity.Node;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,9 +18,21 @@ import java.util.Optional;
 
 @Service
 public class NodeService {
+    @Autowired
+    private KeyService keyService;
+
     private static final String filePath = ResourceUtils.CLASSPATH_URL_PREFIX + "hosts";
 //    private static List<Node> nodeList = loadProperties();
     private static List<Node> nodeList = new ArrayList<>();
+    private RestTemplate restTemplate = new RestTemplate();
+
+    private static NodeService nodeService;
+
+    @PostConstruct
+    public void init() {
+        nodeService = this;
+        nodeService.keyService = this.keyService;
+    }
 
     private static List<Node> loadProperties() {
         synchronized (filePath) {
@@ -88,5 +107,30 @@ public class NodeService {
 
     public List<Node> getNodeArrayList () {
         return nodeList;
+    }
+
+    public List<Node> addNodesFromCA() {
+        List<Node> list = new ArrayList<>();
+
+        String url = "http://localhost:8080/CA/all";
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+        String body = responseEntity.getBody();
+        JSONArray jsonArray = JSONArray.parseArray(body);
+        for(Object obj : jsonArray) {
+            JSONObject jObj = (JSONObject)obj;
+            String address = (String)jObj.get("address");
+            String[] splits = address.split(":");
+            String ip = splits[0];
+            int port = Integer.parseInt(splits[1]);
+            String name = (String)jObj.get("name");
+            String key = (String)jObj.get("public_key");
+            if(key.equals(nodeService.keyService.getPublic_key())) {
+                continue;
+            }
+            Node node = new Node(ip, port, name, key);
+            list.add(node);
+        }
+        nodeList = list;
+        return list;
     }
 }
